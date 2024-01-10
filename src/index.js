b34f06464f28a8a8ee0e3d3f80367892762c977d
NickRayman <nlhrayman@gmail.com>
2024-01-10 17:11:29 -0500

    todo list complete

--- src/index.js
+++ src/index.js
@@ -1,3 +1,45 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+const form=document.querySelector("#createTaskForm");
+let num= 1
+form.addEventListener("submit",(event) =>{
+  event.preventDefault();
+  console.log("submit working");
+  console.log(event.target);
+  let text=document.querySelector("input").value;
+  console.log(text)
+  let header=document.createElement("li");
+  header.className="todos"
+  header.textContent=text;
+  document.querySelector("ol").appendChild(header);
+
+  let del = document.createElement("button");
+  del.textContent="X"
+  let arrayContent= document.querySelector("ol").lastElementChild;
+  console.log(arrayContent);
+  arrayContent.appendChild(del);
+
+  del.addEventListener("click",() => {
+    arrayContent.remove();
+  })
+
+  const prio= form.priority;
+  console.log(prio)
+
+  if(prio.value==="high"){
+    header.style.color="red";
+  }
+  else if(prio.value==="medium"){
+    header.style.color="blue";
+  }
+  else if(prio.value==="low"){
+    header.style.color="green";
+  }
+  else if(prio.value==="very low"){
+    header.style.color="light green";
+  }
+  
+  num +=1
+
 });
+
+
+
